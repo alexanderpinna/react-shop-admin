8b57eb47f9bab84810a2eb79e23274cca3843d47
alex919090 <alex919090@gmail.com>
2022-05-12 18:17:19 -0400

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
       <img src="https://play.tailwindcss.com/img/beams.jpg" className="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2" width={1308} />
-      <div className="absolute inset-0 bg-[url(/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]" />
+      <div className="absolute inset-0 bg-[url(https://play.tailwindcss.com/img/grid.svg)] bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]" />
       <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
         <div className="mx-auto max-w-md">
           <img src="https://play.tailwindcss.com/img/logo.svg" className="h-6" alt={"Tailwind Play"} />
